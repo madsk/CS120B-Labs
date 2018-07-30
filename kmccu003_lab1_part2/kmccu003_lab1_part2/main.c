6f57d9785ded3e4d64c38c4eaedcfd739fd8e8b8
madsk <kmccu003@ucr.edu>
2018-07-30 15:17:27 -0700

    lab 1 part 1
    part 1

---
+++ kmccu003_lab1_part2/kmccu003_lab1_part2/main.c
@@ -0,0 +1,18 @@
+/*
+ * kmccu003_lab1_part2.c
+ *
+ * Created: 7/30/2018 3:14:03 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
