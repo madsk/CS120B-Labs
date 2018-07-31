8c29f38c6fcd96e715131a95a54a854753a43dfd
madsk <kmccu003@ucr.edu>
2018-07-31 15:21:53 -0700

    part 1

--- lab2/kmccu003_lab2_part1/kmccu003_lab2_part1/kmccu003_lab2_part1.c
+++ lab2/kmccu003_lab2_part1/kmccu003_lab2_part1/kmccu003_lab2_part1.c
@@ -4,17 +4,51 @@
  * Created: 7/31/2018 2:30:39 PM
  * Author : ucrcse
  */ 
-
 #include <avr/io.h>
 
+// Bit-access function
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs,
+	
+	// initialize to 0s
+	unsigned char tmpB = 0x00; // intermediate variable used for port updates
+	unsigned char tmpA = 0x00;
+	unsigned char count = 0x00;
+	
+	while(1)
+	{
+		// 1) Read Inputs and assign to variables
+		tmpA = PINA; // Mask PINA to only get the bit you are interested in
+		tmpB = PINB;
 		
-    }
+		count = 0; //reset
+		// 2) Perform Computation
+		
+		for(unsigned char i = 0; i < 8; i++) {
+			if (GetBit(tmpA, i)) { // add up 1's in A, get each bit up to i
+				count++;
+			}
+		}
+		
+		for(unsigned char i = 0; i < 8; i++) {
+			if (GetBit(tmpB, i)) { // add up 1's in B
+				count++;
+			}
+		}
+		
+		// 3) write results to port
+		PORTC = count; //result of 1's in A & B
+	}
 }
 
-//Count the number of 1s on ports A and B and output that number on port C. 
+//Count the number of 1s on ports A and B and output that number on port C.
