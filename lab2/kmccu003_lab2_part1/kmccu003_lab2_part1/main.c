46c68f46c656f94fd89c923eefc3fa87d7e98986
madsk <kmccu003@ucr.edu>
2018-07-31 14:32:31 -0700

    lab2 folder

---
+++ lab2/kmccu003_lab2_part1/kmccu003_lab2_part1/main.c
@@ -0,0 +1,20 @@
+/*
+ * kmccu003_lab2_part1.c
+ *
+ * Created: 7/31/2018 2:30:39 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
+//Count the number of 1s on ports A and B and output that number on port C. 
