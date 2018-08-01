bac67bce1f4df98f5f01540d71c030d62c160cb6
madsk <kmccu003@ucr.edu>
2018-08-01 14:52:04 -0700

    part1

--- lab3/kmccu003_lab3_part1/kmccu003_lab3_part1/kmccu003_lab3_part1.c
+++ lab3/kmccu003_lab3_part1/kmccu003_lab3_part1/kmccu003_lab3_part1.c
@@ -7,31 +7,66 @@
 
 #include <avr/io.h>
 
-enum States{Init, s1, s2} state;
+enum States{Init, s0, s1} state;
 	
 unsigned char button = 0;
 unsigned char tmpB = 0;
 
-void Tick {
+void Tick() {
+	
 	switch(state) { //transitions
 		case Init:
-			state = Init;
+			state = s0;
 			break;
-		case Init:
-			state
+			
+		case s0:
+			if (button) { //button pressed
+				state = s1;
+			}
+			else { //button not pressed stay in s1
+				state = s0;
+			}
+			break;
 		
-		
-		
-		
-		
+		case s1:
+			if (button) { //second press
+				state = s1;
+			}	
+			break;
 	}
+	
+	switch(state) { //actions
+		case Init:
+			tmpB = 0x01; //bit 0
+			PORTB = tmpB;
+			break;
+			
+		case s0:
+			tmpB = 0x02; //bit1
+			PORTB = tmpB;
+			break;
+			
+		case s1:
+			tmpB = 0x01;
+			PORTB = tmpB;
+			break;
+			
+		default:
+			break;
+	}
+}
 
 int main(void) {
 	
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
+	
+	state = Init;
+	tmpB = 0;
 
 	while(1) {
 		void Tick();
 	}
+	
+	return 0;
 }
