63674a6b95c1a0fbb45ceec4f5493d2434552e22
madsk <kmccu003@ucr.edu>
2018-07-31 17:35:16 -0700

    header

--- lab1/kmccu003_lab1_part1/kmccu003_lab1_part1/kmccu003_lab1_part1.c
+++ lab1/kmccu003_lab1_part1/kmccu003_lab1_part1/kmccu003_lab1_part1.c
@@ -1,9 +1,12 @@
-/*
- * kmccu003_lab1_part1.c
- *
- * Created: 7/30/2018 1:47:12 PM
- * Author : Kiana McCullough & Keilani Conner
- */ 
+/*	  Author : Kiana McCullough
+ * 
+ *    Partner(s) Name & E-mail: Keilani Conner, kconn006@ucr.edu
+ *    Lab Section: B21
+ *    Assignment: Lab # 1 Exercise # 1
+ *    
+ *    I acknowledge all content contained herein, excluding template or example
+ *    code, is my own original work.
+ */
 
 #include <avr/io.h>
 
--- lab1/kmccu003_lab1_part3/kmccu003_lab1_part3/kmccu003_lab1_part3.c
+++ lab1/kmccu003_lab1_part3/kmccu003_lab1_part3/kmccu003_lab1_part3.c
@@ -1,9 +1,12 @@
-/*
- * GccApplication1.c
- *
- * Created: 7/31/2018 12:32:58 PM
- * Author : Kiana McCullough & Keilani Conner
- */ 
+/*	  Author : Kiana McCullough
+ * 
+ *    Partner(s) Name & E-mail: Keilani Conner, kconn006@ucr.edu
+ *    Lab Section: B21
+ *    Assignment: Lab # 1 Exercise # 3
+ *    
+ *    I acknowledge all content contained herein, excluding template or example
+ *    code, is my own original work.
+ */
 
 #include <avr/io.h>
 
