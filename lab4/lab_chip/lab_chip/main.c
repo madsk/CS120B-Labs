aa982def514b6397ac100536b062922b17e01b4d
madsk <kmccu003@ucr.edu>
2018-08-05 15:57:20 -0700

    test

---
+++ lab4/lab_chip/lab_chip/main.c
@@ -0,0 +1,17 @@
+/*
+ * lab_chip.c
+ *
+ * Created: 8/5/2018 3:45:41 PM
+ * Author : Kiana
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
+	while(1)
+	{
+		PORTB = 0x0F; // Writes port B's 8 pins with 00001111
+	}
+}
