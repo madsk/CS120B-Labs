bc9f308ebebae5bb9a16ffc0451b4b46efca1608
madsk <kmccu003@ucr.edu>
2018-08-07 12:02:05 -0700

    part 1

--- lab5/kmccu003_lab5_part1/kmccu003_lab5_part1/kmccu003_lab5_part1.c
+++ lab5/kmccu003_lab5_part1/kmccu003_lab5_part1/kmccu003_lab5_part1.c
@@ -43,26 +43,70 @@ void TimerSet(unsigned long M) {
 	_avr_timer_cntcurr = _avr_timer_M;
 }
 
-enum States {INIT,  } state;
+enum States {INIT, led_0, led_1, led_2} state;
 
+void Tick() {
+	switch(state) {
+		case INIT:
+			state = led_0;
+			break;
+			
+		case led_0:
+			state = led_1;
+			break;
+		
+		case led_1:
+			state = led_2;
+			break;
+		
+		case led_2:
+			state = led_0;
+			break;
+	}
+	
+	switch(state) { //actions
+		
+		case INIT:
+			break;
+		
+		case led_0:
+			PORTC = 0x01;
+			break;
+		
+		case led_1:
+			PORTC = 0x02;
+			break;
+		
+		case led_2:
+			PORTC = 0x04;
+			break;
+			
+		default:
+			break;
+		
+	}
+}
 
 int main(void)
 {
     /* Replace with your application code */
-	DDRB = 0xFF;
-	PORTB = 0x00;
+	DDRC = 0xFF; //set port C to output
+	PORTC = 0x00; //init port C to 0's
 	
 	TimerSet(1000);
 	TimerOn();
-	unsigned char tmpB = 0x00;
+	//unsigned char tmpB = 0x00;
 	
 	state = INIT;
 	
     while (1) 
     {
-		tmpB = ~tmpB;
-		PORTB = tmpB;
-		while(!TimerFlag);
+		//tmpB = ~tmpB;
+		//PORTB = tmpB;
+		
+		Tick();
+		
+		while(!TimerFlag); //wait 1 sec
 		TimerFlag = 0;
     }
 }
