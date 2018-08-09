4ba3b6f3d968e0f03cf9bba3f67862b098201fff
madsk <kmccu003@ucr.edu>
2018-08-09 12:57:17 -0700

    part 2

---
+++ lab6/kmccu003_lab6_part2/kmccu003_lab6_part2/kmccu003_lab6_part2.c
@@ -0,0 +1,233 @@
+/*
+ * kmccu003_lab6_part2.c
+ *
+ * Created: 8/9/2018 12:51:13 PM
+ * Author : Kiana
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "io.c"
+
+volatile unsigned char TimerFlag = 0;
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() { //timer/counter controller reg
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80;
+}
+
+void TimerOff() {
+	TCCR1B = 0x00;
+}
+
+void TimerISR() {
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+	_avr_timer_cntcurr--;
+	
+	if(_avr_timer_cntcurr == 0) {
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+//start
+enum States{START, INIT, INC, DEC, RESET, PRESS, RELEASE} state;
+
+unsigned char button_0 = 0x00;
+unsigned char button_1 = 0x00;
+unsigned char counter;
+
+void Tick() {
+	
+	button_0 = ~PINA & 0x01; //inc
+	button_1 = ~PINA & 0x02; //dec
+	
+	switch(state) { //transitions
+
+		case START:
+		state = INIT;
+		break;
+		
+		case INIT:
+		
+		if(button_0 && !button_1) { //button 1 pressed
+			state = INC;
+		}
+		
+		else if(!button_0 && button_1) { //button 2 pressed
+			state = DEC;
+		}
+		
+		else if(button_0 && button_1) { //both pressed
+			state = RESET;
+		}
+		else if(!button_0 && !button_1){ //stay
+			state = INIT;
+		}
+		break;
+		
+		
+		case INC:
+		
+		if(button_0 && button_1) { //both pressed
+			state = RESET;
+		}
+		
+		/*else {
+			state = RELEASE;
+		}*/
+		
+		else if(!button_0 && !button_1) {
+			state = RELEASE;
+		}
+		
+		else if(button_0 && !button_1) { //if still button_0 keep incrementing
+			state = INC;
+		}
+		
+		break;
+		
+		case DEC:
+		
+		if(button_0 && button_1) { //both pressed
+			state = RESET;
+		}
+		
+		/*else {
+			state = RELEASE;
+		}*/
+		
+		else if(!button_0 && !button_1) { //if released
+			state = RELEASE;
+		}
+		
+		else if(!button_0 && button_1) { //if still button keep decrementing
+			state = DEC;
+		}
+		
+		break;
+		
+		case RESET:
+		if(button_0 && button_1) { //both pressed, stay
+			state = RESET;
+		}
+		
+		else if(!button_0 && !button_1) { //neither
+			state = INIT;
+		}
+		
+		else if(!button_0 && button_1) { //dec
+			state = DEC;
+		}
+		
+		else if(button_0 && !button_1) { //inc
+			state = INC;
+		}
+		break;
+		
+		case PRESS:
+		if(button_0 && button_1) { //both pressed, stay
+			state = RESET;
+		}
+		
+		else if(!button_0 && button_1) { //dec
+			state = DEC;
+		}
+		
+		else if(button_0 && !button_1) { //inc
+			state = INC;
+		}
+		else {
+			state = PRESS;
+		}
+		break;
+		
+		case RELEASE:
+		if(!button_0 && !button_1) { //released, wait for press
+			state = PRESS;
+		}
+		
+		else if(button_0 && button_1) { //reset
+			state = RESET;
+		}
+		
+		else {
+			state = RELEASE;
+		}
+		
+		default:
+		break;
+		
+	} //end transitions
+	
+	switch(state) { //actions
+		
+		case INIT:
+		break;
+		LCD_Cursor(1); //positions the cursor on the LCD display
+		LCD_WriteData(counter + '0');
+		
+		case INC:
+		if(counter < 9) {
+			counter += 1;
+		}
+		LCD_Cursor(1); //positions the cursor on the LCD display
+		LCD_WriteData(counter + '0'); //Writes a char at the position the cursor is currently in
+		break;
+		
+		case DEC:
+		if(counter > 0) {
+			counter -= 1;
+		}
+		LCD_Cursor(1); //positions the cursor on the LCD display
+		LCD_WriteData(counter + '0');
+		break;
+		
+		case RESET:
+		counter = 0x00;
+		LCD_Cursor(1); //positions the cursor on the LCD display
+		LCD_WriteData(counter + '0');
+		break;
+		
+		default:
+		break;
+	}
+}
+
+int main(void) {
+	
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs
+	DDRD = 0xFF; PORTD = 0x00; // Configure port D's 8 pins as outputs
+	
+	state = INIT;
+	//tmpC = 0x07;
+	
+	LCD_init(); //initialize LCD
+	
+	TimerSet(1000); //1 sec
+	TimerOn();
+
+	while(1) {
+		Tick();
+		while(!TimerFlag) {};
+			TimerFlag = 0;
+	}
+	
+	return 0;
+}
+
