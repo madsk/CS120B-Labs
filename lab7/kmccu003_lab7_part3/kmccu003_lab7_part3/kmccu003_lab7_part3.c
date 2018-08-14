b6eb91bf5b6ce66a5f6f16b8b4bdb8e9790c07a6
madsk <kmccu003@ucr.edu>
2018-08-14 12:13:24 -0700

    lab 7

---
+++ lab7/kmccu003_lab7_part3/kmccu003_lab7_part3/kmccu003_lab7_part3.c
@@ -0,0 +1,20 @@
+/*
+ * kmccu003_lab7_part3.c
+ *
+ * Created: 8/14/2018 11:58:31 AM
+ * Author : Kiana
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+/*To the previous exercise's implementation, connect your speaker's red wire to PB4 and black wire to
+ground. Add a third task that toggles PB4 on for 2 ms and off for 2 ms as long as a switch on PA2
+is in the on position.*/
