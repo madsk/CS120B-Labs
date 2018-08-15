3daf0b07f97da7a3aafd90d6e247808fa37cd3bb
madsk <kmccu003@ucr.edu>
2018-08-15 14:29:18 -0700

    part 3

---
+++ lab8/kmccu003_lab8_part2/kmccu003_lab8_part2/kccu003_lab8_part2.c
@@ -0,0 +1,47 @@
+/*
+ * kmccu003_lab8_part2.c
+ *
+ * Created: 8/15/2018 1:12:14 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	// ADEN: setting this bit enables analog-to-digital conversion.
+	// ADSC: setting this bit starts the first conversion.
+	// ADATE: setting this bit enables auto-triggering. Since we are
+	//        in Free Running Mode, a new conversion will trigger whenever
+	//        the previous conversion comple
+}
+
+int main(void)
+{
+	/* Replace with your application code */
+	DDRA = 0x00; PORTA = 0xFF; //input potentiometer
+	DDRB = 0xFF; PORTB = 0x00; //LEDs [7:0]
+	DDRD = 0xFF; PORTD = 0x00; //LEDs [1:0]
+	
+	unsigned char tmpB = 0x00;
+	unsigned char tmpD = 0x00;
+	
+	ADC_init();
+	
+	while (1)
+	{
+		
+		unsigned short adc = ADC; // Value of ADC register now stored in variable adc
+
+		tmpB = (char)adc;
+		tmpD = (char)(adc >> 8);
+		
+		PORTB = tmpB;
+		PORTD = tmpD;
+	}
+}
+
+
+/*Revise Part 1 by replacing the potentiometer with a photoresistor and 330
+ohm resistor. Take note of the highest ADC value displayed when the photoresistor is exposed to light,
+and the lowest ADC value displayed when the photoresistor is deprived of all light. These values will be used for the remaining lab exercises.*/
---
+++ lab8/kmccu003_lab8_part3/kmccu003_lab8_part3/kmccu003_lab8_part3.c
@@ -0,0 +1,53 @@
+/*
+ * kmccu003_lab4_part3.c
+ *
+ * Created: 8/15/2018 1:41:13 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	// ADEN: setting this bit enables analog-to-digital conversion.
+	// ADSC: setting this bit starts the first conversion.
+	// ADATE: setting this bit enables auto-triggering. Since we are
+	//        in Free Running Mode, a new conversion will trigger whenever
+	//        the previous conversion comple
+}
+
+int main(void)
+{
+	/* Replace with your application code */
+	DDRA = 0x00; PORTA = 0xFF; //input potentiometer
+	DDRB = 0xFF; PORTB = 0x00; //LEDs [7:0]
+	DDRD = 0xFF; PORTD = 0x00; //LEDs [1:0]
+	
+	/*unsigned char tmpB = 0x00;
+	unsigned char tmpD = 0x00;*/
+	unsigned long MAX = 0x327;
+	
+	ADC_init();
+	
+	while (1)
+	{
+		
+		unsigned short adc = ADC; // Value of ADC register now stored in variable adc
+
+		
+		if(adc >= MAX/2) {
+			PORTB = 0x00;
+		}
+		else if (adc < MAX/2) {
+			PORTB = 0x01;
+		}
+	}
+}
+
+/*Design a system where an LED is illuminated if enough light is detected from the photo resistor.
+
+Criteria:
+If the result of the ADC is >= MAX/2, the LED is illuminated.
+If the result of the ADC is < MAX/2, the LED is turned off.
+Notes:
+MAX is the highest ADC value observed from part 2 of the lab.*/
